df690c5167e88eba398adda6af79e187f95c161e
yousafi <40589224+yousafi@users.noreply.github.com>
2019-01-14 16:52:28 +0430

    Login Modal Front-End

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Authintication</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body text-center">
+                  <div class="card mb-0" style="max-width: 100%">
+      <h5 class="text-uppercase text-center">Login</h5>
+      <br><br>
+
+      <form>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Username">
+        </div>
+
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="Password">
+        </div>
+
+        <div class="form-group flexbox py-10">
+          <label class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" checked>
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Remember me</span>
+          </label>
+
+          <a class="text-muted hover-primary fs-13" href="#">Forgot password?</a>
+        </div>
+
+        <div class="form-group">
+          <button class="btn btn-bold btn-block btn-primary" type="submit">Login</button>
+        </div>
+      </form>
+
+      <div class="divider">Or Sign In With</div>
+      <div class="text-center">
+        <a class="btn btn-circular btn-sm btn-facebook mr-4" href="#"><i class="fa fa-facebook"></i></a>
+        <a class="btn btn-circular btn-sm btn-google mr-4" href="#"><i class="fa fa-google"></i></a>
+        <a class="btn btn-circular btn-sm btn-twitter" href="#"><i class="fa fa-twitter"></i></a>
+      </div>
+
+      <hr class="w-30">
+
+      <p class="text-center text-muted fs-13 mt-20">Don't have an account? <a href="page-register.html">Sign up</a></p>
+    </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
